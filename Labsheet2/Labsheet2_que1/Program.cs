ee883d90f9cba4fc9fa7709506ad51f28a0287f1
Meth Wijesundara <methmihisara@gmail.com>
2024-02-12 02:44:24 +0530

    Add project files.

---
+++ Labsheet2/Labsheet2_que1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Book book1 = new Book("Harry potter", "JK Rowling");
+
+            Console.WriteLine(book1.Title);
+            Console.WriteLine(book1.Author);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Labsheet2/Labsheet2_que3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    public class TemperatureTracker
+    {
+
+        public double[] Temperature = new double[7];
+
+
+
+        public void InputTemp()
+        {
+            for (int i = 0; i < Temperature.Length; i++)
+            {
+                Console.WriteLine($"Enter temperature for day {i + 1}: ");
+                Temperature[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void DisplayTemp()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            TemperatureTracker week1 = new TemperatureTracker();
+
+            week1.InputTemp();
+
+            for (int i = 0; i < 7; i++)
+            {
+
+                Console.WriteLine(week1.Temperature[i]);
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
