a387991b93d6a11918499f298c3c142376c2f608
Meth Wijesundara <methmihisara@gmail.com>
2024-02-12 03:48:15 +0530

    Add project files.

---
+++ Labsheet2_que1/book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut_7
+{
+    internal class Book
+    {
+        //attributes
+        public string Title;
+        public string Author;
+
+        public Book(string title, string author)
+        {
+            this.Title = title;
+            this.Author = author;
+        }
+
+
+
+    }
+}
---
+++ Labsheet2_que2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    internal class BankAccount
+    {
+        private int AccountNumber;
+        public double Balance;
+
+        public BankAccount(double initialBal)
+        {
+            Balance = initialBal;
+        }
+
+        public void deposit(double amount)
+        {
+
+            Balance += amount;
+        }
+
+        static void Main(string[] args)
+        {
+            BankAccount Acc1 = new BankAccount(1000);
+
+            Console.WriteLine("Enter account number: ");
+            Acc1.AccountNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Initial Balance is: " + Acc1.Balance);
+
+            Console.WriteLine("Enter amount to deposit: ");
+
+            double inputAmount = Convert.ToDouble(Console.ReadLine());
+
+            Acc1.deposit(inputAmount);
+
+            Console.WriteLine("New Balance is: " + Acc1.Balance);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
