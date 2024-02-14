1d257e757f2c021ca2f174bee8baee7257879b8e
Meth Wijesundara <methmihisara@gmail.com>
2024-02-14 13:39:21 +0530

    question 4

---
+++ Labsheet2_que4/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet2_que4
+{
+    internal class Product
+    {
+        public string ProductName;
+        public double price;
+
+        public Product(string ProductName, double price)
+        {
+            this.ProductName = ProductName;
+            this.price = price;
+        }
+    }
+}
---
+++ Labsheet2_que4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet2_que4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Product Name: ");
+            string product_name = Console.ReadLine();
+            Console.WriteLine("Enter Product price: ");
+            double price = double.Parse(Console.ReadLine());   
+
+            Product product1 = new Product( product_name ,  price);
+            Console.WriteLine("\nProduct Name is: "+product1.ProductName);
+            Console.WriteLine("Product price is: "+product1.price);
+
+            Console.ReadLine();
+
+        }
+    }
+}
